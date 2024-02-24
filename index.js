9c5f828a84ab9e88adfa8ac3a181616ed378889d
Dustin Li <dustin.li1988@gmail.com>
2024-02-24 11:26:30 +0000

    Setting up Firebase database
    Setting up Firebase database in index.js.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://shoppinglist-b94c9-default-rtdb.europe-west1.firebasedatabase.app"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingList = ref(database, "shoppinglist")
+
+console.log(app)
+
 const inputFieldEl = document.getElementById("input-text")
 const addButtonEl = document.getElementById("add-btn")
 
